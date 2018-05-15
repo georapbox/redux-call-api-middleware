826a3a24b67e078a45af58623eadaa04ef8f2aa5
georapbox <georapbox@gmail.com>
2018-05-15 18:35:55 +0300

    add tests

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 [![npm version](https://img.shields.io/npm/v/@georapbox/redux-call-api-middleware.svg?style=flat-square)](https://www.npmjs.com/package/@georapbox/redux-call-api-middleware)
+[![Travis](https://img.shields.io/travis/georapbox/redux-call-api-middleware/master.svg?style=flat-square)](https://travis-ci.org/georapbox/redux-call-api-middleware)
+[![Codecov](https://img.shields.io/codecov/c/github/georapbox/redux-call-api-middleware/master.svg?style=flat-square)](https://codecov.io/gh/georapbox/redux-call-api-middleware)
 [![Dependencies](https://david-dm.org/georapbox/redux-call-api-middleware.svg?style=flat-square)](https://david-dm.org/georapbox/redux-call-api-middleware)
 [![devDependency Status](https://david-dm.org/georapbox/redux-call-api-middleware/dev-status.svg?style=flat-square)](https://david-dm.org/georapbox/redux-call-api-middleware#info=devDependencies)
 [![npm license](https://img.shields.io/npm/l/@georapbox/redux-call-api-middleware.svg?style=flat-square)](https://www.npmjs.com/package/@georapbox/redux-call-api-middleware)
-
-<!-- [![Travis](https://img.shields.io/travis/georapbox/redux-call-api-middleware/master.svg?style=flat-square)](https://travis-ci.org/georapbox/redux-call-api-middleware.svg?branch=master) -->
-<!-- [![Codecov](https://img.shields.io/codecov/c/github/georapbox/redux-call-api-middleware/master.svg?style=flat-square)](https://codecov.io/gh/georapbox/redux-call-api-middleware) -->
 
 # redux-call-api-middleware
 
@@ -57,10 +56,6 @@ export function loadPosts(userId) {
   }
 }
 ```
-
-## TODO
-
-- Write tests
 
 ## License
 
--- test/callAPIMiddleware.spec.js
+++ test/callAPIMiddleware.spec.js
@@ -7,9 +7,162 @@ const { expect } = chai;
 
 chai.use(sinonChai);
 
-// Replace with actual tests
+const types = {
+  requestType: 'REQUEST_TYPE',
+  successType: 'SUCCESS_TYPE',
+  failureType: 'FAILURE_TYPE'
+};
+
+const create = () => {
+  const store = {
+    dispatch: sinon.spy(),
+    getState: sinon.spy(() => {})
+  };
+  const next = sinon.spy();
+  const invoke = action => callAPIMiddleware(store)(next)(action);
+  return { store, next, invoke };
+};
+
 describe('callAPIMiddleware', () => {
-  it('should pass', () => {
-    expect(1).to.eql(1);
+  it('should pass through next action', () => {
+    const { next, invoke } = create();
+
+    const action = {
+      type: 'TEST_ACTION'
+    };
+
+    invoke(action);
+
+    expect(next).to.have.been.calledWith(action);
+  });
+
+  it('should dispatch actions of type REQUEST_TYPE and SUCCESS_TYPE', () => {
+    const { store, invoke } = create();
+
+    const action = {
+      types,
+      callAPI: () => Promise.resolve(true)
+    };
+
+    invoke(action).then(() => {
+      expect(store.dispatch).to.have.been.calledWith({
+        type: 'SUCCESS_TYPE',
+        response: true
+      });
+    });
+
+    expect(store.dispatch).to.have.been.calledWith({
+      type: 'REQUEST_TYPE'
+    });
+  });
+
+  it('should dispatch actions of type REQUEST_TYPE and FAILURE_TYPE', () => {
+    const { store, invoke } = create();
+
+    const action = {
+      types,
+      callAPI: () => Promise.reject(false)
+    };
+
+    invoke(action).then(
+      () => {},
+      () => {
+        expect(store.dispatch).to.have.been.calledWith({
+          type: 'FAILURE_TYPE',
+          error: false
+        });
+      }
+    );
+
+    expect(store.dispatch).to.have.been.calledWith({
+      type: 'REQUEST_TYPE'
+    });
+  });
+
+  it('should pass payload to dispatched actions', () => {
+    const { store, invoke } = create();
+
+    const action = {
+      types,
+      callAPI: () => Promise.resolve(true),
+      payload: { foo: 'bar' }
+    };
+
+    invoke(action).then(() => {
+      expect(store.dispatch).to.have.been.calledWith({
+        type: 'SUCCESS_TYPE',
+        response: true,
+        foo: 'bar'
+      });
+    });
+
+    expect(store.dispatch).to.have.been.calledWith({
+      type: 'REQUEST_TYPE',
+      foo: 'bar'
+    });
+  });
+
+  it('should not dispatch any actions', () => {
+    const { store, invoke } = create();
+
+    const action = {
+      types,
+      callAPI: () => Promise.reject(false),
+      shouldCallAPI: () => false
+    };
+
+    invoke(action);
+
+    expect(store.dispatch).to.not.have.been.called;
+  });
+
+  it('should throw if types is not plain object', () => {
+    const { invoke } = create();
+
+    const action = {
+      types: ['REQUEST_TYPE', 'SUCCESS_TYPE', 'FAILURE_TYPE'],
+      callAPI: () => Promise.resolve(true)
+    };
+
+    expect(() => invoke(action)).to.throw();
+  });
+
+  it('should throw if not all types provided', () => {
+    const { invoke } = create();
+
+    const action = {
+      types: {
+        requestType: 'REQUEST_TYPE'
+      },
+      callAPI: () => Promise.resolve(true)
+    };
+
+    expect(() => invoke(action)).to.throw();
+  });
+
+  it('should throw if any of the types object value is not a string', () => {
+    const { invoke } = create();
+
+    const action = {
+      types: {
+        requestType: 'REQUEST_TYPE',
+        successType: 'SUCCESS_TYPE',
+        failureType: null
+      },
+      callAPI: () => Promise.resolve(true)
+    };
+
+    expect(() => invoke(action)).to.throw();
+  });
+
+  it('should throw if callAPI is not a function', () => {
+    const { invoke } = create();
+
+    const action = {
+      types,
+      callAPI: Promise.resolve()
+    };
+
+    expect(() => invoke(action)).to.throw();
   });
 });
